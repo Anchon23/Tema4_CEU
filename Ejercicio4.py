7dbfc28d007299b16c87e5234b7d51500c14b446
Anchon23 <113629620+Anchon23@users.noreply.github.com>
2022-12-04 15:16:24 +0000

    commit ej4

---
+++ Ejercicio4.py
@@ -0,0 +1,91 @@
+#Desarrollar un algoritmo numérico iterativo que permita calcular el método de la bisección de una función f(x).
+import math
+
+def f(x):
+    return math.exp(-x)-x
+
+def biseccion(a,b,tol):
+    if f(a)*f(b)>0:
+        print ("No hay raíz en el intervalo")
+    else:
+        while abs(b-a)>tol:
+            c=(a+b)/2.0
+            if f(a)*f(c)<0:
+                b=c
+            else:
+                a=c
+        return c
+    
+
+print(biseccion(0,1,0.0001))
+
+#Desarrollar un algoritmo numérico iterativo que permita calcular el método de la secante de una función f(x).
+
+import math
+
+def f(x):
+    return math.exp(-x)-x
+
+def secante(x0,x1,tol):
+    while abs(f(x1))>tol:
+        x2=x1-f(x1)*(x1-x0)/(f(x1)-f(x0))
+        x0=x1
+        x1=x2
+    return x2
+
+print(secante(0,1,0.0001))
+
+#Desarrollar un algoritmo numérico iterativo que permita calcular el método de Newton-Raphson de una función f(x).
+
+import math
+
+def f(x):
+    return math.exp(-x)-x
+
+def df(x):
+    return -math.exp(-x)-1
+
+def newton_raphson(x,tol):
+    while abs(f(x))>tol:
+        x=x-f(x)/df(x)
+    return x
+
+print(newton_raphson(0,0.0001))
+
+#Comparar los tres algoritmos anteriores para resolver la siguiente función: x3 + x +16 = 0, respecto de la cantidad de iteraciones necesarias por cada método para converger. ¿Cuánto es la diferencia en decimales entre las distintas soluciones?
+
+import math
+
+def f(x):
+    return x**3+x+16
+
+def df(x):
+    return 3*x**2+1
+
+def biseccion(a,b,tol):
+    if f(a)*f(b)>0:
+        print ("No hay raíz en el intervalo")
+    else:
+        while abs(b-a)>tol:
+            c=(a+b)/2.0
+            if f(a)*f(c)<0:
+                b=c
+            else:
+                a=c
+        return c
+
+def secante(x0,x1,tol):
+    while abs(f(x1))>tol:
+        x2=x1-f(x1)*(x1-x0)/(f(x1)-f(x0))
+        x0=x1
+        x1=x2
+    return x2
+
+def newton_raphson(x,tol):
+    while abs(f(x))>tol:
+        x=x-f(x)/df(x)
+    return x
+
+print("Biseccion: ",biseccion(-10,10,0.0001))
+print("Secante: ",secante(-10,10,0.0001))
+print("Newton-Raphson: ",newton_raphson(0,0.0001))
